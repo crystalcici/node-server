0b054e488eb5a2845b2fb730c901d0a7781eaf36
crystalcici <517455797@qq.com>
2020-12-02 23:08:33 +0800

    完成404页面

--- index.ts
+++ index.ts
@@ -12,12 +12,22 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   //处理查询参数用url.parse
   const {pathname, search} = url.parse(path);
   // response.setHeader('Content-type', 'text/html; charset=utf-8');
-  const filename = pathname.substr(1);
-
+  let filename = pathname.substr(1);
+  if (filename === '') {
+    filename = 'index.html';
+  }
   fs.readFile(p.resolve(publicDir, filename), (err, data) => {
     if (err) {
-      response.statusCode = 404;
-      response.end();
+      if (err.errno === -4058) {
+        response.statusCode = 404;
+        fs.readFile(p.resolve(publicDir, '404.html'), (err, data) => {
+          response.end(data);
+        });
+        response.end('你要的文件不存在');
+      } else {
+        response.statusCode = 500;
+        response.end('服务器繁忙');
+      }
     } else {
       response.end(data.toString());
     }
