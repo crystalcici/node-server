2c5f1ad80717bcdabf3d8f4374b0bb84441d712b
crystalcici <517455797@qq.com>
2020-12-02 20:46:38 +0800

    匹配任意文件

--- index.ts
+++ index.ts
@@ -11,39 +11,17 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   const {method, url: path, headers} = request;
   //处理查询参数用url.parse
   const {pathname, search} = url.parse(path);
-  switch (pathname) {
-    case '/index.html':
-      response.setHeader('Content-type', 'text/html; charset=utf-8');
-      fs.readFile(p.resolve(publicDir, 'index.html'), (err, data) => {
-        if (err) {
-          throw err;
-        }
-        response.end(data.toString());
-      });
-      break;
-    case '/style.css':
-      //告知浏览器内容为css
-      response.setHeader('Content-type', 'text/css; charset=utf-8');
-      fs.readFile(p.resolve(publicDir, 'style.css'), (err, data) => {
-        if (err) {
-          throw err;
-        }
-        response.end(data.toString());
-      });
-      break;
-    case '/main.js':
-      response.setHeader('Content-type', 'text/javascript; charset=utf-8');
-      fs.readFile(p.resolve(publicDir, 'main.js'), (err, data) => {
-        if (err) {
-          throw err;
-        }
-        response.end(data.toString());
-      });
-      break;
-    default:
+  // response.setHeader('Content-type', 'text/html; charset=utf-8');
+  const filename = pathname.substr(1);
+
+  fs.readFile(p.resolve(publicDir, filename), (err, data) => {
+    if (err) {
       response.statusCode = 404;
       response.end();
-  }
+    } else {
+      response.end(data.toString());
+    }
+  });
 });
 
 server.listen(8888);
